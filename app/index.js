97e46142f228076c0a485f5e88f43ffb22565b74
Chris McNeese <christophermcneese@mac.com>
2017-06-16 07:25:33 -0500

    delete files

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
 import 'whatwg-fetch';
+// validate.validators.custom = function(value, options, key,attributes) {
+//     console.log(value);
+//     console.log(options);
+//     console.log(key);
+//     console.log(attributes);
+//     return "is totally wrong";
+// }
+
+var constraints = {
+    companyname: {
+        presence: true,
+        exclusion: {
+            within: ["Apple"],
+            message: "'%{value}' is not allowed"
+        }
+    }
+};
+
+// validate({from: null}, constraints);
