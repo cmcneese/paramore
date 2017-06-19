b635c224165d9284f5da6551ff3c4af40a397a8e
Chris McNeese <christophermcneese@mac.com>
2017-06-19 08:43:10 -0500

    validate js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 import 'whatwg-fetch';
-// validate.validators.custom = function(value, options, key,attributes) {
-//     console.log(value);
-//     console.log(options);
-//     console.log(key);
-//     console.log(attributes);
-//     return "is totally wrong";
+validate.validators.custom = function(value, options, key,attributes) {
+    console.log(value);
+    console.log(options);
+    console.log(key);
+    console.log(attributes);
+    return "is totally wrong";
 // }
 
 var constraints = {
